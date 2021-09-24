1e65372a7301181401744d435c0e980b8fcf38d6
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2021-09-24 15:13:35 +0300

    Atnaujimas
    *Bakground paveiksliuka rodo full screen
    *veikia mytukas i registracija
    *veikia mygtukas i testa
    * visa kita susigadino

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
 
@@ -30,3 +32,23 @@ function registracija() {
     document.getElementById("pradinis").innerHTML = "display:none";
     document.getElementById("rekvizitai").innerHTML = "display:block";
   }
+
+var modal = document.getElementById("rekvizitai");
+
+var btn = document.getElementById("regBut");
+
+btn.onclick = function(){
+    pradinis.style.display="none";
+    rekvizitai.style.display="block";
+}
+var modal = document.getElementById("testas");
+
+var testBtn = document.getElementById("testBut");
+
+var taimeris = document.getElementById("taimeris");
+
+testBtn.onclick = function(){
+        rekvizitai.style.display="none";
+        testas.style.display="block";
+        taimeris.style.display="block"
+}
