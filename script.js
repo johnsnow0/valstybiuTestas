e7c5b023e7c9ba9abe6b6ba144fec31b10d49305
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2021-09-24 21:37:47 +0300

    Projekto atnaujimas
    *Istaisytos projekto dizaino klaidos
    *Sudeti 10 klausimu
    *Sutvarkyta navigacija tarp klausimu

--- script.js
+++ script.js
@@ -1,54 +1,124 @@
-
-
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML =  minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
 function registracija() {
-    document.getElementById("pradinis").innerHTML = "display:none";
-    document.getElementById("rekvizitai").innerHTML = "display:block";
-  }
+  document.getElementById("pradinis").innerHTML = "display:none";
+  document.getElementById("rekvizitai").innerHTML = "display:block";
+}
 
 var modal = document.getElementById("rekvizitai");
 
 var btn = document.getElementById("regBut");
 
-btn.onclick = function(){
-    pradinis.style.display="none";
-    rekvizitai.style.display="block";
+btn.onclick = function () {
+  pradinis.style.display = "none";
+  rekvizitai.style.display = "block";
 }
-var modal = document.getElementById("testas");
+var modal = document.getElementById("1klausimas");
 
 var testBtn = document.getElementById("testBut");
 
 var taimeris = document.getElementById("taimeris");
 
-testBtn.onclick = function(){
-        rekvizitai.style.display="none";
-        testas.style.display="block";
-        taimeris.style.display="block"
+testBtn.onclick = function () {
+  rekvizitai.style.display = "none";
+  modal.style.display = "block";
+  taimeris.style.display = "block"
+}
+
+function eitii(klausimas) {
+  var kl1 = document.getElementById("1klausimas");
+  var kl2 = document.getElementById("2klausimas");
+  var kl3 = document.getElementById("3klausimas");
+  var kl4 = document.getElementById("4klausimas");
+  var kl5 = document.getElementById("5klausimas");
+  var kl6 = document.getElementById("6klausimas");
+  var kl7 = document.getElementById("7klausimas");
+  var kl8 = document.getElementById("8klausimas");
+  var kl9 = document.getElementById("9klausimas");
+  var kl10 = document.getElementById("10klausimas");
+
+  if (klausimas == 2) {
+    kl1.style.display = "none"
+    kl2.style.display = "block"
+  }
+  if (klausimas == 3) {
+    kl2.style.display = "none"
+    kl3.style.display = "block"
+  }
+  if (klausimas == 4) {
+    kl3.style.display = "none"
+    kl4.style.display = "block"
+  }
+  if (klausimas == 5) {
+    kl4.style.display = "none"
+    kl5.style.display = "block"
+  }
+  if (klausimas == 6) {
+    kl5.style.display = "none"
+    kl6.style.display = "block"
+  }
+  if (klausimas == 7) {
+    kl6.style.display = "none"
+    kl7.style.display = "block"
+  }
+  if (klausimas == 8) {
+    kl7.style.display = "none"
+    kl8.style.display = "block"
+  }
+  if (klausimas == 9) {
+    kl8.style.display = "none"
+    kl9.style.display = "block"
+  }
+  if (klausimas == 10) {
+    kl9.style.display = "none"
+    kl10.style.display = "block"
+  }
+}
+
+function eitiAtgal(klausimas) {
+  var kl1 = document.getElementById("1klausimas");
+  var kl2 = document.getElementById("2klausimas");
+  var kl3 = document.getElementById("3klausimas");
+  var kl4 = document.getElementById("4klausimas");
+  var kl5 = document.getElementById("5klausimas");
+  var kl6 = document.getElementById("6klausimas");
+  var kl7 = document.getElementById("7klausimas");
+  var kl8 = document.getElementById("8klausimas");
+  var kl9 = document.getElementById("9klausimas");
+  var kl10 = document.getElementById("10klausimas");
+
+  if (klausimas == 2) {
+    kl1.style.display = "block"
+    kl2.style.display = "none"
+  }
+  if (klausimas == 3) {
+    kl2.style.display = "block"
+    kl3.style.display = "none"
+  }
+  if (klausimas == 4) {
+    kl3.style.display = "block"
+    kl4.style.display = "none"
+  }
+  if (klausimas == 5) {
+    kl4.style.display = "block"
+    kl5.style.display = "none"
+  }
+  if (klausimas == 6) {
+    kl5.style.display = "block"
+    kl6.style.display = "none"
+  }
+  if (klausimas == 7) {
+    kl6.style.display = "block"
+    kl7.style.display = "none"
+  }
+  if (klausimas == 8) {
+    kl7.style.display = "block"
+    kl8.style.display = "none"
+  }
+  if (klausimas == 9) {
+    kl8.style.display = "block"
+    kl9.style.display = "none"
+  }
+  if (klausimas == 10) {
+    kl9.style.display = "block"
+    kl10.style.display = "none"
+  }
 }
